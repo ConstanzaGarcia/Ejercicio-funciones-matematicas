ad75cc07cc8948576e0a3a502370d014c6884fb2
ConstanzaGarcia <o.conii.o@gmail.com>
2018-05-28 03:29:56 -0400

    ejercicio funciones-no funcionando

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+let numberone = 36325;
+let numbertwo = 9824;
+let numberthree = 777;
+function add() {
+    return numbertwo + numberthree;
+}
+function myFunction() {
+    document.getElementById("suma").innerHTML = add(); 
+}
+
+function multiply () {
+    return add * numberone;
+}
+function myFunctiontwo() {
+    document.getElementById("multiplicar").innerHTML = multiply(); 
+
+}
